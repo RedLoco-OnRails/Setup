c43ae022921b23b0b6ad6a9d4832e9a90995272f
Sacré Hubert <sacrehubert251200@gmail.com>
2026-06-06 20:14:24 +0200

    Create system-scripts.ps1

---
+++ system-scripts.ps1
@@ -0,0 +1,8 @@
+# Configurer Windows sur les performances maximales (Gaming)
+powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+
+# Désactiver la veille de l'écran et du PC (sur secteur)
+powercfg /change monitor-timeout-ac 0
+powercfg /change disk-timeout-ac 0
+powercfg /change standby-timeout-ac 0
+powercfg /change hibernate-timeout-ac 0
